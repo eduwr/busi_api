dee19dce204722df745e8177153140be2bf19690
eduwr <eduwric@gmail.com>
2021-06-12 13:07:08 -0300

    conf: chage postgres test password

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :busi_api, BusiApi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "secret",
   database: "busi_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
